980d550efc0f4a4e626474115e67895af1dbf82c
fl0rian <rosenkranz.florian@googlemail.com>
2011-05-17 16:55:35 +0200

    veraendert von fl0

--- source/common/src/messages.h
+++ source/common/src/messages.h
@@ -6,6 +6,8 @@
  *
  *  Created on: May 12, 2011
  *      Author: kathrin
+ *  Changed on: May 16, 2011
+ *  	Author: fl0
  */
 
 #include <stdint.h>
@@ -23,7 +25,7 @@
 #define RFC_CATALOGCHANGE        6
 #define RFC_STARTGAME            7
 #define RFC_QUESTIONREQUEST      8 
-#define RFC_QUESTION             9
+#define RFC_QUESTION              9
 #define RFC_QUESTIONANSWERED     10
 #define RFC_QUESTIONRESULT       11
 #define RFC_GAMEOVER             12
@@ -56,6 +58,151 @@ struct msg_header {
 } __attribute__((__packed__));
 
 typedef struct msg_header t_msg_header;
+
+
+/*
+ * Type:1 LoginRequest
+ * Client --> Server
+ */
+typedef struct login_request{
+        const uint8_t type;
+        uint16_t length;
+        char* name;
+}LOGINREQUEST;
+
+
+/*
+ * Type:2 LoginResponseOk
+ * Server --> Client
+ */
+typedef struct login_response_OK{
+        const uint8_t type;
+        uint16_t length;
+        uint8_t clientID;
+}LOGINRESPONSEOK;
+
+
+/*
+ * Type:3 PlayerList
+ * Client --> Server
+ */
+typedef struct player_list{
+        const uint8_t type;
+        uint16_t length;
+        char* players;                   // Das funktioniert so nicht, grad mit Fibu besprochen - Alternativmöglichkeiten?
+}PLAYERLIST,playerls;
+
+
+/*
+ * Type:4 CatalogRequest
+ * Client --> Server
+ */
+typedef struct catalog_request{
+        const uint8_t type;
+        uint16_t length;
+}CATALOGREQUEST,catareq;
+
+
+/*
+ * Type:5 CatalogResponse
+ * Server --> Client
+ */
+typedef struct catalog_response{
+        const uint8_t type;
+        const uint16_t length;
+        char *filename;
+}CATALOGRESPONSE,catares;
+
+
+/*
+ * Type:6 CatalogChange
+ * Server --> Client
+ * Client --> Server
+ */
+
+typedef struct catalog_change{
+        const uint8_t type;
+        uint16_t length;
+        char *filename;
+}CATALOGCHANGE,catach;
+
+
+/*
+ * Type:7 StartGame
+ * Client --> Server
+ */
+typedef struct start_game{
+        const uint8_t type;
+        uint16_t length;
+        char* filename;
+}STARTGAME,startgm;
+
+
+/*
+ * Type:8 QuestionRequest
+ * Client --> Server
+ */
+typedef struct question_request{
+        const uint8_t type;
+        uint16_t length;
+}QUESTIONREQUEST,questionreq;
+
+
+/*
+ * Type:9 Question
+ * Server --> Client
+ */
+typedef struct question{
+        const uint8_t type;
+        uint16_t length;
+        char* data;
+}QUESTION;
+
+
+/*
+ * Type:10 QuestionAnswered
+ * Client --> Server
+ */
+typedef struct question_answered{
+        const uint8_t type;
+        uint16_t length;
+        uint8_t selection;
+}QUESTIONANSWERED;
+
+
+/*
+ * Type:11 QuestionResult
+ * Server --> Client
+ */
+typedef struct question_result{
+        const uint8_t type;
+        uint16_t length;
+        uint8_t selection;
+        uint8_t correct;
+}QUESTIONRESULT;
+
+
+/*
+ * Type:12 GameOver
+ * Server --> Client
+ */
+typedef struct game_over{
+        const uint8_t type;
+        uint16_t length;
+        uint8_t rank;
+}GAMEOVER;
+
+
+/*
+ * Type:255 ErrorWarning
+ * Server --> Client
+ */
+typedef struct error_warning{
+        const uint8_t type;
+        uint16_t length;
+        uint8_t subtype;
+        char* message;
+}ERRORWARNING;
 
 
 #endif
